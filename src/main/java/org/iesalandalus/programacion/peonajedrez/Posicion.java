a3eb94e8be68ea11ad7b0a8ccf7122f198ce18bf
Natii <Natii@DESKTOP-7K17A8A>
2019-11-05 13:24:18 +0100

    Creamos los getters y setters. Validamos las filas y columnas

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
@@ -1,9 +1,35 @@
 package org.iesalandalus.programacion.peonajedrez;
 
 public class Posicion {
-	
+
 	private int fila;
 	private char columna;
-	
+
+	public int getFila() {
+		return fila;
+	}
+
+	public void setFila(int fila) {
+
+		if (fila >= 1 && fila <= 8) {
+			this.fila = fila;
+		} else {
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		}
+
+	}
+
+	public char getColumna() {
+		return columna;
+	}
+
+	public void setColumna(char columna) {
+
+		if (columna >= 'a' && columna <= 'h') {
+			this.columna = columna;
+		} else {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+		}
+	}
 
 }

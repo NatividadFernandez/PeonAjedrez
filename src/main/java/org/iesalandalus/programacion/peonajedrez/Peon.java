980ed1c758e30664f2312f92da1e98f2f3933f50
Natii <Natii@DESKTOP-7K17A8A>
2019-11-05 14:19:16 +0100

    Creacion de constructor con parametro color.

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
@@ -1,40 +1,49 @@
 package org.iesalandalus.programacion.peonajedrez;
 
 public class Peon {
-	
-	Color color;
-	Posicion posicion;
-	
-	//Constructor por defecto
+
+	private Color color;
+	private Posicion posicion;
+
+	// Constructor por defecto
 	public Peon() {
-		
-		this.posicion = new Posicion(7,'d');
+
+		this.posicion = new Posicion(7, 'd');
 		this.color = Color.NEGRO;
 	}
-	
+
+	public Peon(Color color) {
+		if (color.equals(Color.BLANCO)) {
+			this.posicion = new Posicion(2, 'd');
+		} else if (color.equals(Color.NEGRO)) {
+			this.posicion = new Posicion(7, 'd');
+		}
+		this.color = color;
+	}
+
 	// Getters y Setters
 	public Color getColor() {
 		return color;
 	}
-	
+
 	private void setColor(Color color) {
-		
-		if(color == null) {
+
+		if (color == null) {
 			throw new IllegalArgumentException("ERROR: No se puede asignar un color nulo.");
 		}
 		this.color = color;
 	}
+
 	public Posicion getPosicion() {
 		return posicion;
 	}
+
 	private void setPosicion(Posicion posicion) {
-		
-		if(posicion == null) {
+
+		if (posicion == null) {
 			throw new IllegalArgumentException("ERROR: No se puede asignar una columna nula.");
 		}
 		this.posicion = posicion;
 	}
-	
-	
 
 }
